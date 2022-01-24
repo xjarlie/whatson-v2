6bfa147833b2696f885baa4c65600f617d4461ef
Xjarlie <xjarlie@gmail.com>
2022-01-24 20:16:59 +0900

    auth tokens etc

--- routes/app.js
+++ routes/app.js
@@ -1,24 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../conn');
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+    if (await checkToken(req.cookies.AUTH_TOKEN, req.cookies.USERNAME)) {
+        res.render('index');
+    } else {
+        res.redirect('/app/login');
+    }
 });
 
-router.get('/friends', (req, res) => {
-    res.render('friends');
+router.get('/friends', async (req, res) => {
+    if (await checkToken(req.cookies.AUTH_TOKEN, req.cookies.USERNAME)) {
+        res.render('friends');
+    } else {
+        res.redirect('/app/login');
+    }
 });
 
-router.get('/watchlist', (req, res) => {
-    res.render('watchlist');
+router.get('/watchlist', async (req, res) => {
+    if (await checkToken(req.cookies.AUTH_TOKEN, req.cookies.USERNAME)) {
+        res.render('watchlist');
+    } else {
+        res.redirect('/app/login');
+    }
 });
 
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get('/login', async (req, res) => {
+    if (await checkToken(req.cookies.AUTH_TOKEN, req.cookies.USERNAME)) {
+        res.redirect('/app');
+    } else {
+        res.render('login');
+    }
 });
 
-router.get('/signup', (req, res) => {
-    res.render('signup');
+router.get('/signup', async (req, res) => {
+    if (await checkToken(req.cookies.AUTH_TOKEN, req.cookies.USERNAME)) {
+        res.redirect('/app');
+    } else {
+        res.render('signup');
+    }
 });
+
+async function checkToken(givenToken, username) {
+    if (!(givenToken && username)) {
+        return false;
+    }
+    let dbToken = await db.get('auth/users/' + username + '/token');
+    if (dbToken.expires <= Date.now() || dbToken.token != givenToken) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 module.exports = router;
--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,8 @@ const crypto = require('crypto');
 router.post('/signup', async (req, res) => {
     const { username, email, password } = req.body;
 
-    const hash = hashData(password);
-    const data = { username, email, password: hash };
+    const hashed = hashData(password);
+
     const usernameCheck = await db.get('auth/users/' + username);
     const emailCheck = await db.get('auth/emails/' + hashData(email, ' '));
     
@@ -21,27 +21,37 @@ router.post('/signup', async (req, res) => {
         return false;
     }
 
+    const token = generateAuthToken();
+
+    const data = { username, email, password: hashed.hash, salt: hashed.salt, token };
+
     await db.set('auth/users/' + username, data);
     await db.set('auth/emails/' + hashData(email, ' '), true);
 
-    // TODO: Auth tokens idk how
-
     const safeData = { username, email };
-    res.status(201).json({ result: safeData });
+    res.status(201).cookie('AUTH_TOKEN', token.token).cookie('USERNAME', username).json({ result: safeData });
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', async (req, res) => {
+    const { username, password } = req.body;
 
+    const userData = await db.get('auth/users/' + username);
+    if (!userData) {
+        res.status(404).json({ error: 'Username incorrect or account does not exist' });
+        return false;
+    }
 });
 
 function hashData(string, salt) {
     let salto = salt || crypto.randomBytes(16).toString('hex');
     const hash = crypto.pbkdf2Sync(string, salto, 1000, 64, 'sha512').toString('hex');
-    return hash;
+    return { hash, salt: salto };
 }
 
-function generateAuthToken() {
-
+function generateAuthToken(lasts=5184000000 /* 60 days */) {
+    const tokenData = crypto.randomBytes(64).toString('hex');
+    const expires = Date.now() + lasts;
+    return { token: tokenData, expires };
 }
 
 module.exports = router;
